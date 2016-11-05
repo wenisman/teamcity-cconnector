dc917a14f9ed4828cac814a260df9348188afc95
Geoffrey Tsai <geoff.tsai@williamhill.com.au>
2016-11-05 16:04:27 +1100

    addition of the getters to the index

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import * as lib from 'lib';
 
+export default class Connector {
+  constructor (baseUrl, username, password) {
+    this._project = new lib.Project(baseUrl, username, password);
+    this._buildType = new lib.BuildType(baseUrl, username, password);
+    this._vcsRoot = new lib.VcsRoot(baseUrl, username, password);
+  }
+
+  get Project () { return this._project; }
+  get BuildType () { return this._buildType; }
+  get VcsRoot () { return this._vcsRoot; }
+}
+

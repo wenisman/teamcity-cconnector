e65aa7b5dbc987e36c869d1f89ee3316f76a7190
wenisman <wenisman@hotmail.com>
2017-08-10 20:22:02 +1000

    convert the vcs root to use tasks

--- src/lib/vcsroot.js
+++ src/lib/vcsroot.js
@@ -1,76 +1,80 @@
-import Client from './client';
-import R from 'ramda';
+const Client = require('./client');
+const R = require('ramda');
 
-export default class VcsRoot extends Client {
+const baseUri = (args) => {
+  return `${Client.createBaseUrl(args.host)}/vcs-roots`;
+};
 
-  /**
-   * @constructor
-   */
-  constructor (baseUrl, name, password) {
-    super(baseUrl, name, password);
-    this._baseUrl = `${this._baseUrl}/vcs-roots/`;
+const get = (args) => {
+  args.uri = `${baseUri(args)}/name:${args.name}`;
+  return Client.get(args);
+};
+
+/**
+ * Create the request to send to teamcity
+ * @param {string} args.name - (Required) the name of the Vcs Root to create
+ * @param {string} args.projectName - the name of the project the VcsRoot will belong to
+ * @param {string} args.projectId - the name of the project the VcsRoot will belong to
+ * @param {string} args.url - (Required) the url to the repository to use for the vcs root
+ * @param {string} args.branch - the main branch to use for the repository
+ * @param {string} args.vcsType - (Required) the type of vcs connector to use [jetbrains.get|perforce|svn|tfs]
+ */
+const create = (args) => {
+  args.uri = baseUri(args);
+  args.body = createRequestJson(args);
+  return Client.post(args);
+};
+
+const createRequestJson = (args) => {
+  var request = {
+    name: args.name,
+    projectLocator: args.projectName
+                    ? `name:${args.projectName}`
+                    : `id:${args.projectId}`,
+    properties: {
+      property: [
+        { name: 'url', value: args.url }
+      ]
+    },
+    vcsName: args.vcsType
+  };
+
+  if (args.branch) {
+    request.properties.property.push({ name: 'branch', value: args.branch });
   }
 
-  /**
-   * Get the vcs root that matches the name provided
-   * @param {string} args.name - the name of the vcs root to look for
-   */
-  async get (args) {
-    return await super._get({ uri: `${this._baseUrl}name:${args.name}` });
-  }
+  return request;
+};
 
-  /**
-   * Create the request to send to teamcity
-   * @param {string} options.name - (Required) the name of the Vcs Root to create
-   * @param {string} options.projectName - the name of the project the VcsRoot will belong to
-   * @param {string} options.projectId - the name of the project the VcsRoot will belong to
-   * @param {string} options.url - (Required) the url to the repository to use for the vcs root
-   * @param {string} options.branch - the main branch to use for the repository
-   * @param {string} options.vcsType - (Required) the type of vcs connector to use [jetbrains.get|perforce|svn|tfs]
-   */
-  async create (options) {
-    return await super._post({ uri: this._baseUrl }, this._createRequestJson(options));
-  }
+const remove = (args) => {
+  args.uri = `${baseUri(args)}/name:${args.name}`;
+  return Client.del(args);
+};
 
-  async delete (args) {
-    return await super._delete({ uri: `${this._baseUrl}name:${args.name}` });
-  }
-
-  /**
-   * @param {string} args.vcsRootName - the name of the vcsroot
-   * @param {array} args.properties - (Required) the object defining the properties to be set
-   *                                  [{name: name1, value: value1}]
-   */
-  async addProperties (args) {
-    const vcsRoot = await this.get({ name: args.vcsRootName });
+/**
+ * Set the properties on the vcs root
+ * @param {string} args.vcsRootName
+ */
+const addProperties = (args) => {
+  return this.get({ name: args.vcsRootName })
+  .chain(vcsRoot => {
     if (!vcsRoot.isNothing) {
       const existingProperties = R.differenceWith((x, y) => { x.name === y.name; },
-        vcsRoot.get().properties.property,
+        vcsRoot.properties.property,
         args.properties);
       const properties = existingProperties.concat(args.properties);
 
-      return await this._put({uri: `${this._baseUrl}name:${args.vcsRootName}/properties`}, { property: properties });
+      return Client.put({
+        uri: `${baseUri(args)}/name:${args.vcsRootName}/properties`, 
+        body: { property: properties }
+      });
     }
-  }
+  });
+};
 
-  _createRequestJson (args) {
-    var request = {
-      name: args.name,
-      projectLocator: args.projectName
-                      ? `name:${args.projectName}`
-                      : `id:${args.projectId}`,
-      properties: {
-        property: [
-          { name: 'url', value: args.url }
-        ]
-      },
-      vcsName: args.vcsType
-    };
-
-    if (args.branch) {
-      request.properties.property.push({ name: 'branch', value: args.branch });
-    }
-
-    return request;
-  }
-}
+module.exports = {
+  get,
+  create,
+  remove,
+  addProperties
+};
